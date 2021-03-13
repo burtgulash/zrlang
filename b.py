556d08c727dae9035fdd5656c3f5276e6263bc82
Io <me@me.me>
2021-03-13 21:34:31 +0100

    String + escaping

--- b.py
+++ b.py
@@ -1,10 +1,69 @@
 #!/usr/bin/env python3
 
 import sys
+from dataclasses import dataclass
 
-def interpret(buf):
+NIL = None
+
+class Value:
+    def __init__(self, v): self.v = v
+class Var(Value): pass
+class Array(Value): pass
+
+def interpret(block_type, buf):
+    if len(buf) == 0:
+        return NIL
+    elif block_type == "(":
+        # TODO shunting yard
+        y = buf
+        return y
+    elif block_type == "[":
+        return Array(buf)
+    elif block_type == "{":
+        buf[0] = Var(buf[0])
+
+    elif block_type == "[|":
+
+        pass
+
+        #(x, y => {x}, {y} = {{y}}, {{x}})
+        #[| 1 + {| x |} + 3 |]
+        #foo = (x, y -> {|x|} + {y})
+
+
     return buf
 
+def parse_string(i, cs, xs):
+    assert len(cs) == 0
+    escape = False
+    while i[0] < len(xs):
+        i[0] += 1
+        c = xs[i[0]]
+
+        if escape:
+            if c == "n":
+                cs.append("\n")
+            elif c == "t":
+                cs.append("\t")
+            elif c == '"':
+                cs.append(c)
+            elif c == "\\":
+                cs.append(c)
+            else:
+                raise ValueError(f"invalid escape in string: \{c}")
+            escape = False
+            continue
+
+        if c == '"':
+            return "".join(cs)
+        if c == "\\":
+            escape = True
+            continue
+
+        cs.append(c)
+
+    cs = "".join(cs)
+    raise ValueError(f"unterminated string: {cs}")
 
 def parse(i, cs, xs):
     assert not cs
@@ -38,8 +97,13 @@ def parse(i, cs, xs):
 
         if c == " ":
             continue
+        if c == '"':
+            x = parse_string(i, cs, xs)
+            cs.clear()
+            buf.append(x)
         if c == "(":
             x = parse(i, cs, xs)
+            cs.clear()
             buf.append(x)
         elif c in ")\0":
             if len(buf) < 1:
@@ -50,10 +114,40 @@ def parse(i, cs, xs):
                 if len(buf) % 2 == 0:
                     buf = [*buf, None]
 
-            buf = interpret(buf)
+            buf = interpret("(", buf)
             return buf
 
+def toint(x):
+    if x is None:
+        return 0
+    return int(x)
 
+
+def exe_(x):
+    if isinstance(x, list):
+        return exe(x)
+    return x
+
+
+
+def exe(xs):
+    assert len(xs) % 2 == 1
+
+    L = exe_(xs[0])
+    for i in range(1, len(xs), 2):
+        H = exe_(xs[i])
+        R = exe_(xs[i + 1])
+
+        op = {
+            "~": lambda a, b: a + b,
+            "+": lambda a, b: toint(a) + toint(b),
+            "*": lambda a, b: toint(a) * toint(b),
+            "-": lambda a, b: toint(a) - toint(b),
+        }[H]
+
+        L = op(L, R)
+
+    return L
 
 
 
@@ -65,3 +159,6 @@ if __name__ == "__main__":
     print("X", x)
     y = parse(i, cs, x)
     print("Y", y)
+
+    z = exe(y)
+    print("Z", z)
